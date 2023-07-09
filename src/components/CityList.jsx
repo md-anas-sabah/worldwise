cb4f9e2af9c737c9c3929efc5ca1a4f5ea8cd65f
Md Anas Sabah <mdanassabah@gmail.com>
2023-07-10 01:08:24 +0530

    added cities

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,9 +1,13 @@
 import CityItem from "./CityItem";
 import styles from "./CityList.module.css";
 import Spinner from "./Spinner";
+import Message from "./Message";
 
 function CityList({ cities, isLoading }) {
   if (isLoading) <Spinner />;
+
+  if (!cities.length)
+    return <Message message="Add your city by clicking on your city on the map" />;
 
   return (
     <ul className={styles.cityList}>
